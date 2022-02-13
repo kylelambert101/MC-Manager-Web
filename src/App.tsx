9aeda45ee7bce50b8f058affc4b1b9c7878db887
Kyle Lambert <kylelambert101@gmail.com>
2022-02-13 12:33:00 -0500

    Copy in decoder app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import DecoderPage from "./decoder/DecoderPage";
+
+function App() {
+  return (
+    <div className="App">
+      <DecoderPage />
+    </div>
+  );
+}
+
+export default App;
